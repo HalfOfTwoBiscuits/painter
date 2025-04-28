0d1ed50e2ef00c2d97ac068529c99c235b34cf4c
HalfOfTwoBiscuits <40010399@petroc.ac.uk>
2025-04-28 12:43:43 +0100

    further menu drafting

--- content/game/menu_visual.py
+++ content/game/menu_visual.py
@@ -9,6 +9,11 @@ class MenuVisual(VisualHandler):
     this is because the options can vary but the logic is the same.'''
 
     __OPTIONS_PER_PAGE = 10 # To correspond with the number keys 0-9
+    __PADDING_PX = 4 # Used for option padding and rounding of corners
+    __LINE_SIZE = 1 # Size in pixels of lines separating options
+
+    __TEXT_COL = pg.Color(0,0,0)
+    __BG_COL = pg.Color(200,200,200)
 
     # Font data
     __FONT_FILETYPE = '.ttf'
@@ -19,40 +24,107 @@ class MenuVisual(VisualHandler):
 
     __FONT = pg.font.Font(__FONT_PATH, 16)
     __TITLE_FONT = pg.font.Font(__FONT_PATH, 20)
-    
-    __PADDING = 4
 
     def __init__(self, title: str, options: list[str]):
         self.__title = title
         self.__options = options
-        self.__page = 1
+        self.__page_index = 0
         self.__num_pages = ceil(len(options) / self.__class__.__OPTIONS_PER_PAGE)
 
-        self.__width = self.__option_height = 0
-        # Find the width required for the longest option
+        # Find the width required for the longest line in the menu
         # and height required for the tallest.
-        for o in options:
-            w, h = self.__class__.__FONT.size(o)
-            if w > self.__width: self.__width = w
-            if h > self.__option_height: self.__option_height = h
+        # Use the width and height of the title as a starting point
+        option_width, option_height = self.__class__.__TITLE_FONT.size(title)
 
-        # Also consider the width and height of the title.
-        title_w, title_h = self.__class__.__TITLE_FONT.size(title)
-        if title_w > self.__width: self.__width = title_w
-        if title_h > self.__option_height: self.__option_height = title_h
+        # Iterate over options to check if any are wider or taller
+        for index, o in enumerate(options):
+            # Prepend with the key used to select it
+            full_o = self.__prepend(o, index)
 
-        # Add padding
-        self.__width += self.__class__.__PADDING * 2
-        self.__option_height += self.__class__.__PADDING * 2
+            # If an option is the widest or tallest so far, update width/height
+            w, h = self.__class__.__FONT.size(full_o)
+            if w > option_width: option_width = w
+            if h > option_height: option_height = h
+
+        # Add padding to width to find the menu width
+        self.__width = option_width + self.__class__.__PADDING_PX * 2
+
+        # Add padding and the size of a dividing line to height
+        # to find height of a row in the menu.
+        self.__row_height = option_height + self.__class__.__PADDING_PX * 2 + self.__class__.LINE_SIZE
 
         # Find total height: height of all options plus one for the title
-        self.__height = self.__option_height * (len(options) + 1)
+        self.__height = self.__row_height * (len(options) + 1)
 
         # Get window dimensions and subtract the width and height needed,
-        # to find the position of the topleft.
+        # to find the position of the topleft and bottom.
         win_width, win_height = self.__class__._window_dimensions
         self.__left_edge = (win_width - self.__width) // 2
         self.__top_edge = (win_height - self.__height) // 2
+        self.__bottom_edge = self.__height - self.__top_edge
+
+    def __prepend(self, option: str, index: int):
+        '''Add the key pressed to select the option to the front of it.
+        Returns the text displayed on the menu for the option.'''
+        index_of_page = index // self.__class__.__OPTIONS_PER_PAGE
+        key_for_option = index - index_of_page * self.__class__.__OPTIONS_PER_PAGE
+        return f'{key_for_option}) {option}' 
 
     def draw(self):
-        pg.draw.rect()
+        # Draw background rect
+        pg.draw.rect(self.__class__._window, self.__class__.__BG_COL,
+                     (self.__left_edge, self.__top_edge, self.__width, self.__height),
+                     border_radius = self.__class__.__PADDING_PX)
+        
+        # Draw title
+        self.__draw_menu_row(self.__title, self.__top_edge, is_title=True)
+
+        # Find the options being shown on the current page
+        cur_options = self.__options[self.__page_index * self.__class__.__OPTIONS_PER_PAGE - 1:]
+
+        # Iterate over options, incrementing top_y by option_height from top to bottom of the menu
+        # If we are on the last page and there are fewer options left than OPTIONS_PER_PAGE
+        # then zip() will truncate the range of y positions.
+        for o, top_y in zip(cur_options,
+            range(self.__top_edge + self.__row_height, self.__bottom_edge, self.__row_height)):
+            full_o = self.__prepend(o)
+            self.__draw_menu_row(full_o, top_y)
+
+    def __draw_menu_row(self, content: str, top: int, is_title: bool=False):
+        '''Render the given content string, with padding separating it from
+        the left edge of the menu and from the given y position for the top.
+        If is_title is True, use the larger font.
+        Underneath, draw a line.'''
+
+        # Find topleft corner of the option
+        left = self.__left_edge + self.__class__.PADDING_PX
+        top += self.__class__.PADDING_PX
+
+        # Select font to use
+        font = is_title and self.__class__.__TITLE_FONT or self.__class__.__FONT        
+
+        text_surf = font.render(content, True, self.__class__.__TEXT_COL)
+        self.__class__._window.blit(text_surf, (left, top))
+
+    def option_chosen(self, number_pressed: int):
+        '''Return a string indicating the option chosen when the given number key was pressed.
+        Only has a point when options may or will use more than one page,
+        so the response to a key might change depending on visual state.'''
+
+        # If the 0 key was pressed, select the 10th shown option
+        if number_pressed == 0: index_to_add = self.__class__.__OPTIONS_PER_PAGE
+        else: index_to_add = number_pressed - 1
+
+        return self.__options[index_to_add + self.__page_index * self.__class__.__OPTIONS_PER_PAGE]
+
+    def next_page(self):
+        '''Increment the page number to show later options.
+        Does nothing if the menu is on the last page.'''
+        if self.__page_index < self.__num_pages:
+            self.__page_index += 1
+
+    def prev_page(self):
+        '''Decrement the page number to show earlier options.
+        Does nothing if the menu is on the first page.'''
+        if self.__page_index > 0:
+            self.__page_index -= 1
