ee1369a194bee8dd1a5f374943754f32145aad62
HalfOfTwoBiscuits <40010399@petroc.ac.uk>
2025-05-11 14:51:24 +0100

    unsuccessful attempts to make pygbag work

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from src.game.main import Game, setup_state, setup_window
+# /// script
+# dependencies = [
+#   "PyYAML",
+#   "pygame-ce"
+# ]
 import asyncio
+from src.game.game import Game, setup_state, setup_window
+
 if __name__ == '__main__':
     InitialState = setup_state()
     window = setup_window()
--- main_offline.py
+++ main_offline.py
@@ -1,4 +1,4 @@
-from src.game.main import Game, setup_state, setup_window
+from src.game.game import Game, setup_state, setup_window
 if __name__ == '__main__':
     InitialState = setup_state()
     window = setup_window()
