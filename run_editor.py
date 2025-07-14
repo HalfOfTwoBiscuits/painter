23383ed40b5286169b6bfb463507c298d4045250
HalfOfTwoBiscuits <40010399@petroc.ac.uk>
2025-07-14 16:40:20 +0100

    don't denote the initial state as a class in the main scripts, since a string is now used

--- run_editor.py
+++ run_editor.py
@@ -1,7 +1,11 @@
 from src.editor.editor import Editor
 from src.startup_utility import setup_state, setup_window
-if __name__ == '__main__':
-    InitialState = setup_state(editor=True)
+
+def main():
+    initial_state = setup_state(editor=True)
     window = setup_window(True)
-    e = Editor(InitialState, window)
+    e = Editor(initial_state, window)
     e.main()
+
+if __name__ == '__main__':
+    main()
